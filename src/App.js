c78fc747d8ec46888662bbff84a04cea9ba3f99a
nicholashui8 <nicholashui8@gmail.com>
2020-07-18 13:06:59 -0700

    switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Resume from './components/Resume';
 import Projects from './components/Projects'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { HashRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
