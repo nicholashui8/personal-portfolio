1ab85da28f6a32405131eb6c294f742fb2480529
nicholashui8 <nicholashui8@gmail.com>
2020-07-21 21:28:05 -0700

    Updates

---
+++ precache-manifest.f0f5db6267d9e2449b5d39f8a254d8c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f93e7c6def8a6e19e30f233bc516a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72ed4e0be3944bc0bc5",
+    "url": "/static/css/main.19f468fa.chunk.css"
+  },
+  {
+    "revision": "dd5518a086c2fc4f3e95",
+    "url": "/static/js/2.a93e803e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a93e803e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72ed4e0be3944bc0bc5",
+    "url": "/static/js/main.cc20f235.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  },
+  {
+    "revision": "43d60c726a9f3c6191d0fcc2b359e15e",
+    "url": "/static/media/Nicholas-Hui-Resume.43d60c72.pdf"
+  },
+  {
+    "revision": "f3c5b7fa90c9a4b1118fb4a58010b2f9",
+    "url": "/static/media/food-finder-preview.f3c5b7fa.png"
+  },
+  {
+    "revision": "8b76ef35fd65ab72d9208acc3dd2e0e4",
+    "url": "/static/media/portfolio-preview.8b76ef35.png"
+  },
+  {
+    "revision": "86479483c0bb8be5e08a0b731788891e",
+    "url": "/static/media/youtube-synchronizer-preview.86479483.png"
+  }
+]);
