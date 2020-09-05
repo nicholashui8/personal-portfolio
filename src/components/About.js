5819d55c1f785d3fba2b12bf39b76046611e7f36
nicholashui8 <nicholashui8@gmail.com>
2020-09-05 02:38:05 -0700

    switch to materialui and update resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,35 @@
 import React, { Component } from 'react'
+import { Grid } from '@material-ui/core';
+
+const container = {
+    height: '500px',
+    width: '100%',
+    textAlign: 'center',
+    display: 'inline-block',
+    marginTop: '75px',
+    backgroundColor: 'white',
+};
+
+export default function About() {
+    return (
+        <div style={container}>
+            <Grid container >
+                <Grid item xs={12}>
+                    <h1>I'm a third year student studying computer science at San Jose State University.</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <h1>I'm currently seeking Summer 2021 internships!</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <h1>In my spare time I like playing badminton and drinking boba with friends.</h1>
+                </Grid>
+            </Grid>
+        </div>
+        
+    )
+}
+
+/*
 class About extends Component {
     render() {
         return (
@@ -19,3 +50,4 @@ const paragraphStyle = {
 
 }
 export default About
+*/
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,136 @@ import portfolioPreview from '../screenshots/portfolio-preview.png';
 import foodFinderPreview from '../screenshots/food-finder-preview.png';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import '../App.css'
-
+import {Grid} from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { faNodeJs as Nodejs } from '@fortawesome/free-brands-svg-icons'
 import { faReact as react } from '@fortawesome/free-brands-svg-icons'
+const container = {
+    height: '1000px',
+    width: '100%',
+    textAlign: 'center',
+    display: 'inline-block',
+    marginTop: '70px',
+    backgroundColor: 'white',
+};
+const nodejsColor = {
+    color: 'green',
+}
+const reactColor = {
+    color: '#61DBFB',
+}
+const projectTitle = {
+    backgrounColor: 'red',
+    color: '#FF6961',
+    fontSize: '27px',
+    letterSpacing: '1.75px',
 
+}
+const gridStyle = {
+    backgroundColor: 'white',
+    width: '60%',
+    margin: 'auto',
+    textAlign: 'left',
+}
+export default function Projects() {
+    return (
+        <div style={container}>
+            <Grid container spacing={10}>
+                <Grid item xs={12}>
+                    <div className='imagePreview'>
+                        <Link class='projectLinks' to="/projects" onClick={ () => window.open('https://github.com/nicholashui8/youtube_livestream')}>
+                            <img className='image-style' src={youtubeSynchronizerPreview} alt="Logo"/>
+                        </Link>
+                    </div>
+                </Grid>
+                <Grid container spacing={1} style={gridStyle}>
+                    <Grid item xs={12}>
+                        <p1  style={projectTitle} className='projectDescription'>Youtube Synchronizer</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p1 className='projectDescription'>This web application allows multiple friends to stream Youtube videos together</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p1 className='projectDescription'>Node.js <FontAwesomeIcon style={nodejsColor} className='iconStyle' icon={Nodejs}/> + Express</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link class='projectLinks' to="/projects" onClick={ () => window.open('https://youtube-streaming.herokuapp.com/')} >
+                            <p2>https://youtube-streaming.herokuapp.com/</p2>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link class='projectLinks' to="/projects" onClick={ () => window.open('https://github.com/nicholashui8/youtube_livestream')} >
+                            <p3>https://github.com/nicholashui8/youtube_livestream</p3>
+                        </Link>
+                    </Grid>
+                </Grid>
+
+
+
+                <Grid item xs={12}  style={{marginTop: '100px',}}>
+                    <div className='imagePreview'>
+                        <Link class='projectLinks' to="/projects" onClick={ () => window.open('https://github.com/nicholashui8/food-finder')}>
+                            <img className='image-style' src={foodFinderPreview} alt="Logo"/>
+                        </Link>
+                    </div>
+                </Grid>
+                <Grid container spacing={2} style={gridStyle}>
+                    <Grid item xs={12}>
+                        <p1 style={projectTitle} className='projectDescription'>Food Finder</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p1 className='projectDescription'>This web application allows the user to search for nearby food</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p1 className='projectDescription'>React.js <FontAwesomeIcon style={reactColor} className='iconStyle' icon={react}/> + Node.js <FontAwesomeIcon  style={nodejsColor}className='iconStyle' icon={Nodejs}/> + Express</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link class='projectLinks' to="/projects" onClick={ () => window.open('https://github.com/nicholashui8/food-finder')} >
+                            https://github.com/nicholashui8/food-finder
+                        </Link>
+                    </Grid>
+                </Grid>
+
+
+
+                <Grid item xs={12}  style={{marginTop: '100px',}}>
+                    <div className='imagePreview'>
+                        <Link class='projectLinks' to="/projects/personal-portfolio">
+                            <img className='image-style' src={portfolioPreview} alt="Logo"/>
+                        </Link>
+                    </div>
+                </Grid>
+                <Grid container spacing={1} style={gridStyle}>
+                    <Grid item xs={12}>
+                        <p1 style={projectTitle}  className='projectDescription'>Personal Portfolio</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p1 className='projectDescription'>This is the website you're currently on!</p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p1 className='projectDescription'>React.js  <FontAwesomeIcon style={reactColor} className='iconStyle' icon={react}/></p1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link class='projectLinks' to="/" onClick={ () => window.open('https://nicholashui8.github.io/personal-portfolio/')}>
+                            nicholashui.me
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link class='projectLinks' to="/" onClick={ () => window.open('https://github.com/nicholashui8/personal-portfolio')}>
+                            https://github.com/nicholashui8/personal-portfolio
+                        </Link>
+                    </Grid>
+                </Grid>
+    
+            </Grid>
+
+            
+        </div>
+    )
+}
+
+/*
 class Projects extends Component {
     render() {
         return (
@@ -94,3 +218,4 @@ const projectTitle = {
 
 }
 export default Projects
+*/
